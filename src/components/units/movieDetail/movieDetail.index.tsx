257451fe9e1a0b4342b4e19fd908f62b7f4d5d29
devgyuri <gksmf910@gmail.com>
2024-05-18 05:57:16 +0900

    add video player (ReactPlayer)

--- src/components/units/movieDetail/movieDetail.index.tsx
+++ src/components/units/movieDetail/movieDetail.index.tsx
@@ -5,7 +5,7 @@ import {
 } from "../../commons/hooks/queries/useQueryFetchActorImage";
 import { useFetchActorImage } from "../../commons/hooks/rest/useFetchActorImage";
 import { IMovieDetailProps } from "./movieDetail.types";
-import ReactPlayer from "react-player/lazy";
+import ReactPlayer from "react-player";
 import {
   IQuery,
   IQueryFetchActorImageArgs,
@@ -21,6 +21,11 @@ export default function MovieDetail(props: IMovieDetailProps): JSX.Element {
 
   const { urls } = useFetchActorImage({ names: actorNames });
 
+  const vodUrl = props.data?.vods[0].url.replace(
+    "trailerPlayPop?pFileNm=",
+    "play/",
+  );
+
   return (
     <>
       Movie Detail Page
@@ -29,21 +34,28 @@ export default function MovieDetail(props: IMovieDetailProps): JSX.Element {
           <img key={idx} src={`https://image.tmdb.org/t/p/original/${el}`} />
         );
       })}
-      <iframe
+      {/* <iframe
         src="https://www.kmdb.or.kr/trailer/trailerPlayPop?pFileNm=MK060910_P02.mp4"
         title="video"
         width="100%"
         height="500"
         frameBorder="0"
-      ></iframe>
-      {/*<ReactPlayer url="https://www.kmdb.or.kr/trailer/trailerPlayPop?pFileNm=MK060910_P02.mp4" /> */}
+      ></iframe> */}
+      <ReactPlayer url={vodUrl} controls={true} />
       {/* <video
-        src="https://www.kmdb.or.kr/trailer/trailerPlayPop?pFileNm=MK060910_P02.mp4"
+        src="https://www.kmdb.or.kr/trailer/play/MK060910_P02.mp4"
         width="300"
         controls
         preload="none"
         crossOrigin="anonymous"
       ></video> */}
+      <video
+        src={vodUrl}
+        width="300"
+        controls
+        preload="none"
+        crossOrigin="anonymous"
+      ></video>
     </>
   );
 }
