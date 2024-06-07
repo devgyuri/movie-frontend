ac2371d32faabce9d5e98c94c34f22d25ff9f825
devgyuri <gksmf910@gmail.com>
2024-06-08 07:48:31 +0900

    combine create & update user

--- pages/signUp/index.tsx
+++ pages/signUp/index.tsx
@@ -1,12 +1,10 @@
 import LayoutNavigation from "../../src/components/commons/layout/navigation/LayoutNavigation.index";
-import SignUp from "../../src/components/commons/signUp/SignUp.index";
 import UserInfoWrite from "../../src/components/units/userInfoWrite/UserInfoWrite.index";
 
 export default function signUpPage(): JSX.Element {
   return (
     <>
       <LayoutNavigation />
-      {/* <SignUp /> */}
       <UserInfoWrite isEdit={false} />
     </>
   );
