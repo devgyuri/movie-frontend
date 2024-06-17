9fcd0631a8c9d311ca59d91f02bbe42535497a8c
devgyuri <gksmf910@gmail.com>
2024-06-18 01:35:02 +0900

    modify nav bar z-index

--- src/components/commons/layout/body/LayoutBody.styles.ts
+++ src/components/commons/layout/body/LayoutBody.styles.ts
@@ -2,4 +2,5 @@ import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
   padding-top: 80px;
+  z-index: 0;
 `;
--- src/components/commons/layout/navigation/LayoutNavigation.styles.ts
+++ src/components/commons/layout/navigation/LayoutNavigation.styles.ts
@@ -5,7 +5,7 @@ export const Wrapper = styled.header`
   position: fixed;
   top: 0;
   width: 100%;
-  z-index: 1;
+  z-index: 2;
   padding: 40px;
   height: 80px;
   background-color: #212121;
